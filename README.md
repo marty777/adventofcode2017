1d9b4c3018b5ca5550c9a9b76ba8b681575d0c7a
Martin Thorne <martin.thorne@gmail.com>
2021-07-09 09:02:10 -0600

    day 24 cleanup

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ To run:
 Puzzle input and example input files should be placed in the ./Data directory. e.g. ./Data/Day01/input.txt, ./Data/Day02/input.txt, etc.
 
 Note that all tests expect an input text file, even for single-value inputs on puzzle days that don't provide one for download. Insert your input value in the first line of a text file and save to the appropriate directory.
+
+![alt text](https://github.com/marty777/adventofcode2017/blob/main/complete.png "All done")
--- Sources/adventofcode2017/Days/Day24.swift
+++ Sources/adventofcode2017/Days/Day24.swift
@@ -7,41 +7,7 @@ class Day24: PuzzleDay {
 		var b:Int
 	}
 	
-	static func recurse(components:[Component], used:[Bool], chain:[Int], level:Int) -> Int {
-		var prev:Int
-		if chain.count == 0 {
-			prev = 0
-		}
-		else {
-			prev = chain.last!
-		}
-		var best = 0
-		for c in chain {
-			best += c
-		}
-		for i in 0..<components.count {
-			if (components[i].a == prev || components[i].b == prev) && !used[i] {
-				var nextChain = chain
-				var nextUsed = used
-				nextUsed[i] = true
-				if components[i].a == prev {
-					nextChain.append(components[i].a)
-					nextChain.append(components[i].b)
-				}
-				else {
-					nextChain.append(components[i].b)
-					nextChain.append(components[i].a)
-				}
-				let result = recurse(components:components, used:nextUsed, chain:nextChain, level:level+1)
-				if result > best {
-					best = result
-				}
-			}
-		}
-		return best
-	}
-	
-	static func recurse2(components:[Component], used:[Bool], chain:[Int], level:Int) -> (Int,Int) {
+	static func recurse(components:[Component], used:[Bool], chain:[Int], level:Int, part2:Bool) -> (Int,Int) {
 		var prev:Int
 		if chain.count == 0 {
 			prev = 0
@@ -67,11 +33,18 @@ class Day24: PuzzleDay {
 					nextChain.append(components[i].b)
 					nextChain.append(components[i].a)
 				}
-				let (resultLength, resultStrength) = recurse2(components:components, used:nextUsed, chain:nextChain, level:level+1)
-				if resultLength > bestLength || (resultLength == bestLength && resultStrength > bestStrength) {
-					bestLength = resultLength
-					bestStrength = resultStrength
-					//print(level, nextChain, "\t", bestLength, bestStrength)
+				let (resultLength, resultStrength) = recurse(components:components, used:nextUsed, chain:nextChain, level:level+1, part2:part2)
+				if part2 {
+					if resultLength > bestLength || (resultLength == bestLength && resultStrength > bestStrength) {
+						bestLength = resultLength
+						bestStrength = resultStrength
+					}
+				}
+				else {
+					if(resultStrength > bestStrength) {
+						bestStrength = resultStrength
+						bestLength = resultLength
+					}
 				}
 			}
 		}
@@ -93,8 +66,8 @@ class Day24: PuzzleDay {
 			components.append(Component(a:Int(split[0])!, b:Int(split[1])!))
 		}
 		let used = [Bool](repeating:false,count:components.count)
-		let part1 = recurse(components:components, used:used, chain:[], level:0)
-		let( _,part2) = recurse2(components:components, used:used, chain:[], level:0)
+		let (_,part1) = recurse(components:components, used:used, chain:[], level:0, part2:false)
+		let (_,part2) = recurse(components:components, used:used, chain:[], level:0, part2:true)
 		print("Part 1:", part1)
 		print("Part 2:", part2)
 	}
